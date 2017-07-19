99b810b31b0c6f9b6b56b7947f1853739831b6a5
Graham Gilbert <graham.gilbert@airbnb.com>
2017-07-19 12:44:42 -0700

    Require the notify

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,5 +37,6 @@ define apple_package (
     checksum      => $checksum,
     force_install => $force_install,
     downgrade     => $downgrade,
+    require => Notify["${title} - ${new_downgrade}"]
   }
 }
